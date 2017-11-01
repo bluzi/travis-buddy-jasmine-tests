e8699f515cc7956dbe4cd30ed369e9b842c6e301
Eliran Pe'er <eliran013@gmail.com>
2017-11-01 19:55:31 +0200

    initial jasmine tests and travis

---
+++ spec/OneSpec.js
@@ -0,0 +1,9 @@
+describe('one', function() {
+    it('should be equal to two', function() {
+        expect(1).toBe(2);
+    });
+
+    it('should be greather than zero', function() {
+        expect(1).toBeGreaterThan(0);
+    })
+});
